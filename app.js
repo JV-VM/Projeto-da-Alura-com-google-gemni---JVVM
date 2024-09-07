2fd602cc03e5cbdf46b5a3949ce8c3b1066af754
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-09-07 07:18:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function pesquisar(){
+    let section = document.getElementById("resultado-da-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    
+    let resultados = "" ;
+    for(let pagina of paginas){
+        if(pagina.titulo.includes(campoPesquisa)){
+            resultados += `
+                <div class="regiao-resultado">
+                <h2 class="texto-comum"
+                    <a>${pagina.titulo}</a>
+                </h2>
+                <p class="texto-comum">
+                    ${pagina.descricao}
+                </p>
+                <a href="${pagina.link}">Mais informações</a>
+                </div>
+            `
+        }
+    }
+
+    section.innerHTML = resultados;
+
+}
---
+++ dados.js
@@ -0,0 +1,12 @@
+let paginas = [
+    {
+    titulo:"Mihserie Morgan",
+    descricao: "Mihserie Morgan, também conhecido como \"Mihserie Psycho\" é o protagonista titular da obram, ele é conhecido pelo seu poder de viagem no tempo cujo usa com objetivo de impedir os incidentes de Izaris Fall.",
+    link:"pagina-mihserie.html"
+    },
+    {
+        titulo: "Os incidentes de Izaris Fall",
+        descricao: "Uma série de acidentes fatais bizarros que levarão 53 vitmas, conectados por um serial killer que morreu a meses atrás e o protagonista que acabou de voltar.",
+        link: "pagina-incidentes-de-izaris-fall.html"
+    }
+];
